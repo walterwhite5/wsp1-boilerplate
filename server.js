7ed5908adedae8b3fdb9570aa64e1daf2ba93250
vincent <vincentnt.nilsson@elev.ga.ntig.se>
2025-03-25 11:00:18 +0100

    steg ???? (session tror jag)

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import "dotenv/config"
 import logger from "morgan"
 import express from "express"
 import nunjucks from "nunjucks"
+import session from "express-session"
 import bcrypt from "bcrypt"
+
 
 const app = express()
 const port = 3000
@@ -12,22 +14,59 @@ nunjucks.configure("views", {
   express: app,
 })
 
+app.use(session({
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { sameSite: true }
+}))
+
 app.use(express.static("public"))
 
 app.use(logger("dev"))
 
+
+
 app.get("/", (req, res) => {
+  if (req.session.views) {
+    req.session.views++
+  } else {
+    req.session.views = 1
+  }
   res.render("index.njk",
-    { title: "Qvixter", message: "Best service, legit." }
+    { title: "Test", message: "Funkar?", views: req.session.views }
   )
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+//app.get("/", (req, res) => {
+
+  //let myPlaintextPassword = "SecurePass100"
+  //bcrypt.hash(myPlaintextPassword, 10, function (err, hash) {
+  //  console.log(hash)
+  //})
+
+  //res.render("index.njk",
+  //  { title: "Qvixter", message: "Best service, legit." }
+  //)
+//})
+
+
+
+app.get("/login", (req,res) => {
+  // visa loginformulär
+})
+
+app.post("/login", (req, res) => {
+  // req.body
+  console.log(req.body)
+  // installera body-parser
+  // hämta data från databasen för att sedan
+  // jämföra lösenorden med bcrypt.compare()
+  // om lösenorden stämmer, result == True
+  // då sätter du req.session.loggedin = True
 })
 
 
-let myPlaintextPassword = "SecurePass100"
-bcrpyt.hash(myPlaintextPassword, 10, function(err, hash) {
-  console.log(hash)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
